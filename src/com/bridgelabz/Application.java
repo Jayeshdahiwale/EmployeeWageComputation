42f55c6944aa7b35d02569b7f93f8d15f0d040e6
Jayeshdahiwale <jayjdahivale@gmail.com>
2022-03-02 13:51:11 +0530

    file uploaded

--- src/com/bridgelabz/Application.java
+++ src/com/bridgelabz/Application.java
@@ -4,32 +4,43 @@ public class Application {
     static final int WAGE_PER_HOUR=20;
     static final int FULL_DAY_HOUR=8;
     static final int PARTIAL_DAY_HOUR=4;
-	
+	static final int MAX_WORKING_DAYS=20;
 	public static void main(String[] args) {
 	System.out.println("Welcome to employee wage computation program.");
 	
-	
-
-	
-	int a= (int)Math.floor(Math.random()*10)%3;
-	switch(a) {
-	case 1:
-		System.out.println("Employee is present");
+	int monthlyWage=0;
+    int days=1;
+	while(days<=20) {
 		
-		System.out.println("Daily Wage :Rs."+FULL_DAY_HOUR * WAGE_PER_HOUR);
-		break;
-	
-	case 2:
-		System.out.println("Part Time Employee is present");
-		System.out.println("Daily Wage :Rs."+PARTIAL_DAY_HOUR * WAGE_PER_HOUR);
-		break;
+		int a= (int)Math.floor(Math.random()*10)%3;
 		
-	
-	
-	default:
-		System.out.println("Employee is absent");
-		break;
+		switch(a) {
+		
+		case 1:
+			System.out.println("Employee is present");
+			int dailyWage=FULL_DAY_HOUR * WAGE_PER_HOUR;
+			System.out.println("Daily Wage :Rs."+dailyWage);
+			monthlyWage=monthlyWage+dailyWage;
+			break;
+		
+		case 2:
+			System.out.println("Part Time Employee is present");
+			int partialDailyWage=PARTIAL_DAY_HOUR * WAGE_PER_HOUR;
+			System.out.println("Daily Wage :Rs."+partialDailyWage);
+			monthlyWage=monthlyWage+partialDailyWage;
+			break;
+			
+		
+		
+		default:
+			System.out.println("Employee is absent");
+			break;
+		
+		}
+		days++;
+		
 	}
+	System.out.println("Total monthly wage :Rs."+monthlyWage);
 	
 	
 }
