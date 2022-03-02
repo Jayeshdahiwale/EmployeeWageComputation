57d7485807732b23cf1fd9b4b8b40c75a05666a6
Jayeshdahiwale <jayjdahivale@gmail.com>
2022-03-02 11:46:14 +0530

    Modified file uploaded

--- src/com/bridgelabz/Application.java
+++ src/com/bridgelabz/Application.java
@@ -5,5 +5,12 @@ public class Application {
 	
 	public static void main(String[] args) {
 	System.out.println("Welcome to employee wage computation program.");
+	int a= (int)Math.floor(Math.random()*10)%2;
+	if(a==1) {
+		System.out.println("Employee is present");
+	}
+	else {
+		System.out.println("Employee is absent");
+	}
 }
 }
